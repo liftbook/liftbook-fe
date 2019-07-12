86b6bbcec999420b25e169125b1acc74f13c7cd1
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-07-11 22:35:13 -0400

    installed dependencies, set up React Router, built out frame of login form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+import LoginForm from './components/Login/LoginForm'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header" />
+      {/* <header className="App-header" /> */}
+      <Route exact path="/" render={props => <LoginForm {...props} />} />
     </div>
   );
 }
---
+++ src/components/Login/LoginForm.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+class LoginForm extends React.Component {
+    state = {
+        username: "",
+        password: ""
+    };
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        const credentials = this.state;
+        axios
+            .post("", credentials)
+            .then(res => {
+                const username = res.data.username;
+                const token = res.data.token;
+                localStorage.setItem("token", token);
+                localStorage.setItem("username", username);
+                this.props.history.push("/home");
+                console.log("Logged In");
+            })
+            .catch(err => console.log(err.response))
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>LiftBook</h1>
+                <div className='login'>
+                    <h2>Log In</h2>
+                    <form className='loginForm' onSubmit={this.handleSubmit}>
+                        <div className='inputs'>
+                            <input
+                                type='username'
+                                name='username'
+                                placeholder='Username'
+                                value={this.state.username}
+                                onChange={this.handleChange}
+                            />
+                            <input
+                                type='password'
+                                name='password'
+                                placeholder='Password'
+                                value={this.state.password}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <button onClick={this.handleSubmit} className='loginButton'>Log In</button>
+                    </form>
+                    <div className='newAccountLink'>
+                        <p>Don't have an account yet?</p>
+                        <Link to='/signup' className='signupLink'>Sign Up</Link>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default LoginForm;
