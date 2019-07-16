13e42b8d11529381ff013817b2f08cd09883bf3d
davorin <47928470+davorinpiljic@users.noreply.github.com>
2019-07-15 21:28:44 -0600

    Merge action file

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,17 @@ export const LOGIN_START = 'LOGIN_START';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 
+export const FETCHING = "FETCHING";
+export const CREATING = "CREATING";
+export const DELETING = "DELETING";
+export const UPDATING = "UPDATING";
+export const NOT_UPDATING = "NOT_UPDATING";
+
+export const DATA_SUCCESS = "DATA_SUCCESS";
+
+export const DATA_FAIL = "DATA_FAIL";
+
+
 export const login = creds => dispatch => {
     dispatch({ type: LOGIN_START });
 
@@ -20,3 +31,15 @@ export const login = creds => dispatch => {
             dispatch({ type: LOGIN_FAILURE })
         })
 }
+export const getUsers = () => dispatch => {
+    dispatch({ type: FETCHING });
+    axios
+      .get("https://lift-book.herokuapp.com/api/users")
+      .then(response => {
+        dispatch({ type: DATA_SUCCESS, payload: response.data });
+      })
+      .catch(error => {
+        dispatch({ type: DATA_FAIL, payload: error });
+      });
+  };
+  
--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { login } from '../../actions'
+import { login } from '../../actions/index'
 import { Link } from 'react-router-dom';
 
 import './LoginForm.css'
