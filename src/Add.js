7ee48ebb154c622ce43ad2594403a9fb4b451d66
Poly Goveia <poly.goveia@gmail.com>
2019-07-16 20:08:17 -0700

    style form

--- src/Add.js
+++ src/Add.js
@@ -1,78 +1,68 @@
 import React, { Component } from "react";
 import "./Add.css";
 
-import withStyles from "@material-ui/core/styles/withStyles";
+// material-ui imports
 import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
 import Input from "@material-ui/core/Input";
-import CustomInput from "components/CustomInput/CustomInput.jsx";
-
-// @material-ui/icons
-import Clear from "@material-ui/icons/Clear";
-import Check from "@material-ui/icons/Check";
-// core components
-import customInputStyle from "assets/jss/material-dashboard-react/components/customInputStyle.jsx";
-
-// Material-Ui
-import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 import NativeSelect from "@material-ui/core/NativeSelect";
-import OutlinedInput from "@material-ui/core/OutlinedInput";
-import FilledInput from "@material-ui/core/FilledInput";
-
-import MenuItem from "@material-ui/core/MenuItem";
 import FormHelperText from "@material-ui/core/FormHelperText";
-
-import Select from "@material-ui/core/Select";
-
-const formStyle = {
-  color: "green"
-};
 
 export default class Add extends Component {
   render() {
     return (
       <form>
-        <FormControl>
-          <InputLabel>Name of Exercise</InputLabel>
+        <FormControl className="input-style">
+          <InputLabel>NAME OF EXERCISE</InputLabel>
           <Input />
         </FormControl>
 
         <br />
 
-        <FormControl>
-          <InputLabel>Amount Lifted</InputLabel>
+        <FormControl className="input-style">
+          <InputLabel>WEIGHT LIFTED</InputLabel>
           <Input />
         </FormControl>
 
         <br />
 
-        <FormControl>
-          <InputLabel>Reps completed</InputLabel>
+        <FormControl className="input-style">
+          <InputLabel className="input-label">REPS COMPLETED</InputLabel>
           <Input />
         </FormControl>
 
         <br />
 
-        <FormControl>
-          <InputLabel>Date</InputLabel>
+        <FormControl className="input-style">
+          <InputLabel className="input-label">DATE</InputLabel>
           <Input />
         </FormControl>
 
         <br />
         <br />
 
-        <FormControl>
+        <FormControl className="input-style">
           <NativeSelect>
             <option value="" disabled>
-              Body Part
+              BODY PART
             </option>
-            <option>Bicep</option>
-            <option>Triceps </option>
-            <option>Back</option>
-            <option>Shoulder</option>
+            <option>BICEPS</option>
+            <option>TRICEPS</option>
+            <option>BACK</option>
+            <option>CHEST</option>
+            <option>SHOULDERS</option>
+            <option>ABS</option>
+            <option>LEGS</option>
+            <option>MISC</option>
           </NativeSelect>
-          <FormHelperText>Body Part</FormHelperText>
+          <FormHelperText>BODY PART</FormHelperText>
         </FormControl>
+
+        <br />
+        <br />
+
+        <Button variant="outlined">Submit</Button>
       </form>
     );
   }
