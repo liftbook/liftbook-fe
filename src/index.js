14bdb0f8808c0034f4c12ac5a9afa48e11937549
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-07-20 17:57:31 -0400

    imported composeWithDevTools to index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducer";
