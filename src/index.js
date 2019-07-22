1bc2f69a677152c81e11d7fd5d3de44c385d50e7
Poly Goveia <poly.goveia@gmail.com>
2019-07-22 13:00:11 -0700

    clean up index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
-// import { composeWithDevTools } from "redux-devtools-extension";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducer";
@@ -11,14 +10,10 @@ import reducer from "./reducer";
 import { persistStore, persistReducer } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 import storage from "redux-persist/lib/storage";
-
 import { BrowserRouter as Router } from "react-router-dom";
 
 import "assets/scss/material-kit-pro-react.scss?v=1.7.0";
 
-// import { createBrowserHistory } from "history";
-
-// var hist = createBrowserHistory();
 const persistConfig = {
   key: "root",
   storage
@@ -28,7 +23,6 @@ const persistedReducer = persistReducer(persistConfig, reducer);
 
 const store = createStore(persistedReducer, applyMiddleware(thunk, logger));
 let persistor = persistStore(store);
-// composeEnhancers()
 ReactDOM.render(
   <div>
     <Router>
